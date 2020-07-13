a7e519ff5f4acedc9ab2e487f3f012f25726c49f
ivanjakim <ivan.jakim1002@gmail.com>
2020-07-13 15:43:45 +0200

    Added DataTables and bootbox

--- SistemZaDonaciiNaKrv/App_Start/BundleConfig.cs
+++ SistemZaDonaciiNaKrv/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace SistemZaDonaciiNaKrv
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +21,14 @@ namespace SistemZaDonaciiNaKrv
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/DataTables/dataTables.bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                       "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
