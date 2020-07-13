2f90eef5f36d611fd2a9f8a8579616654c25a799
ivanjakim <ivan.jakim1002@gmail.com>
2020-07-13 17:04:59 +0200

    Added model

---
+++ SistemZaDonaciiNaKrv/Models/BloodBankModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemZaDonaciiNaKrv.Models
+{
+    public class BloodBankModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    
+    }
+}
