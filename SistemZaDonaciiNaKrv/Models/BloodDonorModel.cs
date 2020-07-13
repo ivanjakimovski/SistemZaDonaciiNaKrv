5e79c3dbdd864f8a8d0a384383450c10de6b482f
ivanjakim <ivan.jakim1002@gmail.com>
2020-07-13 14:53:41 +0200

    Added Model

---
+++ SistemZaDonaciiNaKrv/Models/BloodDonorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemZaDonaciiNaKrv.Models
+{
+    public class BloodDonorModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public virtual List<DonationModel>  allDonations { get; set; }
+
+
+        public BloodDonorModel()
+        {
+            this.allDonations = new List<DonationModel>();
+        }
+    }
+}
