329cd088937106197772f1a72d333a3d046aead5
ivanjakim <ivan.jakim1002@gmail.com>
2020-07-13 14:41:32 +0200

    Initial Commit

---
+++ SistemZaDonaciiNaKrv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemZaDonaciiNaKrv.Startup))]
+namespace SistemZaDonaciiNaKrv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
