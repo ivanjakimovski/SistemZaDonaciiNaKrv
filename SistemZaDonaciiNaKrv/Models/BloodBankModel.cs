68c6d5203b0d34f8d65ef0941b3fbca248b43558
ivanjakim <ivan.jakim1002@gmail.com>
2020-07-13 20:37:29 +0200

    Changed model

--- SistemZaDonaciiNaKrv/Models/BloodBankModel.cs
+++ SistemZaDonaciiNaKrv/Models/BloodBankModel.cs
@@ -10,7 +10,7 @@ namespace SistemZaDonaciiNaKrv.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
-        public string Address2 { get; set; }
+        public string Address { get; set; }
 
 
     
