0c55fd0f10265056fd8ec5373864c84655d779ae
ivanjakim <ivan.jakim1002@gmail.com>
2020-09-16 12:04:21 +0200

    Changed login form

--- SistemZaDonaciiNaKrv/Controllers/AccountController.cs
+++ SistemZaDonaciiNaKrv/Controllers/AccountController.cs
@@ -184,13 +184,14 @@ namespace SistemZaDonaciiNaKrv.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
+                    UserManager.AddToRole(user.Id, "Donor");
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
